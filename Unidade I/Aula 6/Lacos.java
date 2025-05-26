096712cf1206e83fc51ca0a6170d009dc7d63929
Andre Santos <euandremas@gmail.com>
2025-05-26 15:11:36 -0300

    Laços de Repetição

---
+++ Unidade I/Aula 6/Lacos.java
@@ -0,0 +1,26 @@
+public class Lacos {
+
+    public static void main(String[] args) {
+
+        // Laço for, repete um numero determinado de vezes
+
+        for (int i = 1; i <= 10; i += 2) {
+            System.out.println("Valor de i: " + i);
+        }
+
+        int x = 1;
+        // Laço while, repete enquanto a condição for verdadeira. Quando for falsa, sai
+        // do laço
+        while (x <= 10) {
+            System.out.println("Valor de x: " + x);
+            x++; // Incrementa x de 2 em 2
+        }
+        int y = 1;
+
+        // Laço do while, repete pelo menos uma vez e depois verifica a condição
+        do {
+            System.out.println("Valor de y: " + x);
+            x++; // Incrementa x de 2 em 2
+        } while (x <= 10);
+    }
+}
