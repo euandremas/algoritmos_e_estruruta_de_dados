e905db7053584de0af985c9f4b50b7bfcea07c04
Andre <euandremas@gmail.com>
2025-06-08 12:09:55 -0300

    Continuação

---
+++ Unidade I/Atividade1/Exercício_6.java
@@ -0,0 +1,16 @@
+// Exercício 6: Números primos entre 1 e 100
+public class Exercicio6 {
+    public static void main(String[] args) {
+        System.out.println("Números primos entre 1 e 100:");
+        for (int i = 2; i <= 100; i++) {
+            boolean primo = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+            if (primo) System.out.print(i + " ");
+        }
+    }
+}
